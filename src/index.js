9054a0f922c41994ae99264c67c631f299679242
LamPhuongZ <lamphuong024@gmail.com>
2023-06-21 12:23:53 +0700

    create my-food-sequelize

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+import rootRouter from './routers/rootRouter';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.listen(8080); 
+app.use("/api", rootRouter);
